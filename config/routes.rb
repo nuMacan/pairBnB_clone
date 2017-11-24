1eca204a40539963250f7a33bdaad9208735f8cd
Nu <nunung.khotimah@ymail.com>
2017-11-24 21:50:55 +0800

    user sign in via facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
  Rails.application.routes.draw do
+  # get 'home/index'
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resource :session, controller: "clearance/sessions", only: [:create]
+  resource :session, controller: "sessions", only: [:create]
 
   
 resources :galleries do 
   resources :photos 
 end 
 
-  resources :users, controller: "clearance/users", only: [:create] do
+  resources :users, controller: "users", only: [:create] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
@@ -17,5 +19,9 @@ end
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
  end
