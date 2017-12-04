b3bded5bb77ee9cd38ac559d961c39cf59fde90a
Nu <nunung.khotimah@ymail.com>
2017-12-04 18:43:23 +0800

    Added enum role to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
 #listing management, organize relation of user and listing 
   has_many :listings
 
+  enum role: [:customer, :moderator, :superadmin]
+
   def self.create_with_auth_and_hash(authentication, auth_hash)
   	user = self.create!(
   		name: auth_hash["name"],
