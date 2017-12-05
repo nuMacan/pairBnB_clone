97a90bae98870543c072445bf064c198e178f732
Nu <nunung.khotimah@ymail.com>
2017-12-05 10:06:19 +0800

    1. re adding omniauth facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   include Clearance::User
 
-#user management for sign in with omniauth(other site,eg facebook or google)
-  has_many :authentications, dependent: :destroy
 
 #listing management, organize relation of user and listing 
   has_many :listings
 
   enum role: [:customer, :moderator, :superadmin]
 
+#user management for sign in with omniauth(other site,eg facebook or google)
+  has_many :authentications, dependent: :destroy
   def self.create_with_auth_and_hash(authentication, auth_hash)
   	user = self.create!(
   		name: auth_hash["name"],
@@ -19,15 +19,10 @@ class User < ApplicationRecord
   	return user 
   end
 
-
 #grab fb_token to access facebook for user data 
   def fb_token 
   	x = self.authentications.find_by(provider:'facebook')
   	return x.token unless x.nill
   end 
-
-
-
-
 
 end
